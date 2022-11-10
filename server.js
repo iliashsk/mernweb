c8fcdfa1710828c46c441a913c42e133b97c4c9b
iliash <iliash.sk462@gmail.com>
2022-11-10 09:05:16 +0530

    mernweb

--- server.js
+++ server.js
@@ -88,13 +88,6 @@ app.get("/api",async(req,res)=>{
  
  });
  */
-if(process.env.NODE_ENV==="production"){
-app.use(express.static('client/build'))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-  })
-}
-
 if ( process.env.NODE_ENV == "production"){
 
     app.use(express.static("client/build"));
